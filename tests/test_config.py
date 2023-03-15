b55085feac4f3f78218ad89e6473d042ac488f4b
pederhan <pederhan@usit.uio.no>
2023-03-15 15:20:42 +0100

    Add pre-root validator for extra config fields

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,30 +1,37 @@
-import os
-import unittest
+import logging
 import tomli
 
 import pytest
-from pydantic.error_wrappers import ValidationError
-
+from pydantic import Extra
 import zabbix_auto_config.models as models
 
 
-def test_sample_config(sample_config):
+def test_sample_config(sample_config: str):
     models.Settings(**tomli.loads(sample_config))
 
 
-def test_invalid_config(sample_config):
+def test_config_extra_field(sample_config: str, caplog: pytest.LogCaptureFixture):
     config = tomli.loads(sample_config)
     config["foo"] = "bar"
-    with pytest.raises(ValidationError) as exc_info:
+    models.Settings(**config)
+    assert len(caplog.records) == 1
+    record = caplog.records[0]
+    assert record.levelname == "WARNING"
+    assert record.levelno == logging.WARNING
+    assert "'foo'" in record.message
+
+
+def test_config_extra_field_allowed(
+    sample_config: str, caplog: pytest.LogCaptureFixture
+):
+    config = tomli.loads(sample_config)
+    config["foo"] = "bar"
+
+    # Allow extra fields for this test
+    original_extra = models.Settings.__config__.extra
+    try:
+        models.Settings.__config__.extra = Extra.allow
         models.Settings(**config)
-    assert exc_info.value.errors() == [
-        {
-            "loc": ("foo",),
-            "msg": "extra fields not permitted",
-            "type": "value_error.extra",
-        }
-    ]
-
-
-if __name__ == "__main__":
-    unittest.main()
+        assert len(caplog.records) == 0
+    finally:
+        models.Settings.__config__.extra = original_extra
